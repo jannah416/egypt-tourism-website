d14775f10ac87850dce12679f237247a52dc80ba
jannah <jannahatef2003@gmail.com>
2024-10-04 05:41:12 +0300

    Add components for Contact Us, Home, Login, and Where to Gpages

---
+++ src/app/pages/where-to-go/where-to-go.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-where-to-go',
+  standalone: true,
+  imports: [],
+  templateUrl: './where-to-go.component.html',
+  styleUrl: './where-to-go.component.css'
+})
+export class WhereToGoComponent {
+
+}
