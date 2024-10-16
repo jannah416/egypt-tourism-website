062c11a62d2d8270c0eba258e7692f83ffe3f853
jannah <jannahatef2003@gmail.com>
2024-10-16 22:32:09 +0300

    add login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { FormControl,FormGroup,ReactiveFormsModule,Validators,} from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+    loginForm: FormGroup;
+
+    constructor() {
+     
+      this.loginForm = new FormGroup({
+        email: new FormControl('info123@gmail.com', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(6),Validators.maxLength(16)]),
+      });
+    }
+  
+  
+    onSubmit() {
+      if (this.loginForm.valid) {
+        console.log('Login successful:', this.loginForm);
+       
+      } else {
+        console.log('Form is invalid');
+      }
+    }
 }
+
+
